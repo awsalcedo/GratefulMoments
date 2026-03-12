a1743ca70d3d1994bce61bb5c83758e0318be76a
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-03-11 22:56:33 -0500

    Crear una vista de entrada de datos

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct MomentEntryView: View {
+    @State private var title = ""
+    @State private var note = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                contentStack
+            }
+            //Para ocultar el teclado al desplazarse fuera de la pantalla
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Grateful For")
+        }
+    }
+    
+    var contentStack: some View {
+        VStack(alignment: .leading) {
+            TextField(text: $title) {
+                Text("Title (Required)")
+            }
+            .font(.title.bold())
+            .padding(.top, 48)
+            Divider()
+            
+            TextField("Log your small wins", text: $note, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .lineLimit(5...Int.max) // al menos 5 lineas y todo el texto que deseen
+        }
+        .padding()
     }
 }
 
