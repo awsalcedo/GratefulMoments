80722575778a873bd0182de271372b10665f91de
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-03-11 22:55:18 -0500

    Crear una vista de entrada de datos

---
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 11/3/26.
+//
+
+import SwiftUI
+
+struct MomentEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentEntryView()
+}
