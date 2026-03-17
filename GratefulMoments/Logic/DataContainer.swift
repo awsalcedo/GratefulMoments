8c51fc1e01a1ca1a026569a129e0e9f1e4708935
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-03-16 21:18:44 -0500

    Crea un contenedor de datos. Usa SwiftData para almacenar las entradas. Crea un DataContainer compartido para acceder a SwiftData y proporcionar datos de ejemplo para previsualizaciones y desarrollo.

--- GratefulMoments/Logic/DataContainer.swift
+++ GratefulMoments/Logic/DataContainer.swift
@@ -5,4 +5,64 @@
 //  Created by ALEX WLADIMIR SALCEDO SILVA on 16/3/26.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+// Clase para configurar SwiftData
+// Tener un lugar centralizado para el contenedor del modelo evita que la configuración de datos afecte a tus vistas.
+
+// Marcar la clase `DataContainer` con `@MainActor` garantiza que cualquier interacción con el contenedor desde tus vistas se realice en el hilo principal, lo cual es necesario para las actualizaciones de la interfaz de usuario
+
+// La macro `@Observable` le indica a SwiftUI que supervise tu `DataContainer` en busca de cambios. Agregas el contenedor al entorno para que el contenedor del modelo y cualquier propiedad futura de `DataContainer` estén disponibles a través de la jerarquía de vistas.
+
+@Observable
+@MainActor
+class DataContainer {
+    let modelContainer: ModelContainer
+
+
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+
+
+    init(includeSampleMoments: Bool = false) {
+        let schema = Schema([
+            Moment.self,
+        ])
+
+
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: includeSampleMoments)
+
+
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+
+
+            if includeSampleMoments {
+                loadSampleMoments()
+            }
+            try context.save()
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+
+
+    private func loadSampleMoments() {
+        for moment in Moment.sampleData {
+            context.insert(moment)
+        }
+    }
+}
+
+private let sampleContainer = DataContainer(includeSampleMoments: true)
+
+// extensión de vista para simplificar la adición del contenedor de datos de ejemplo a tus vistas previas.
+extension View {
+    func sampleDataContainer() -> some View {
+        self
+            .environment(sampleContainer)
+            .modelContainer(sampleContainer.modelContainer)
+    }
+}
--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -5,59 +5,59 @@
 //  Created by ALEX WLADIMIR SALCEDO SILVA on 11/3/26.
 //
 
-import SwiftUI
 import PhotosUI
+import SwiftUI
 
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
     @State private var imageData: Data?
     @State private var newImage: PhotosPickerItem?
-    
+
     var body: some View {
         NavigationStack {
             ScrollView {
                 contentStack
             }
-            //Para ocultar el teclado al desplazarse fuera de la pantalla
+            // Para ocultar el teclado al desplazarse fuera de la pantalla
             .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Grateful For")
         }
     }
-    
-    //PhotosPickerItem representa la imagen seleccionada de la biblioteca de fotos. La usarás para guardar la imagen. PhotosPicker funciona como un botón, con una gran área para pulsar, y su icono se personaliza con el color de acento de tu aplicación.
-    
+
+    // PhotosPickerItem representa la imagen seleccionada de la biblioteca de fotos. La usarás para guardar la imagen.
+    // PhotosPicker funciona como un botón, con una gran área para pulsar, y su icono se personaliza con el color de
+    // acento de tu aplicación.
+
     private var photoPicker: some View {
         PhotosPicker(selection: $newImage) {
             Group {
                 if let imageData, let uiImage = UIImage(data: imageData) {
                     Image(uiImage: uiImage)
-                    .resizable()
-                    .scaledToFit()
-            } else {
-                
-                Image(systemName: "photo.badge.plus.fill")
-                    .font(.largeTitle)
-                    .frame(height: 250)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(white: 0.4, opacity: 0.32))
-            }
-            
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                }
             }
             .clipShape(RoundedRectangle(cornerRadius: 16))
         }
-        //Responda a la selección de fotos con un modificador .onChange.
+        // Responda a la selección de fotos con un modificador .onChange.
         .onChange(of: newImage) {
-            guard let newImage else {return}
+            guard let newImage else { return }
             // carga la foto seleccionada en la aplicación mediante una tarea asíncrona.
             Task {
-                // La función `loadTransferable` transfiere la imagen de la biblioteca de Fotos a tu aplicación en el formato de datos solicitado.
+                // La función `loadTransferable` transfiere la imagen de la biblioteca de Fotos a tu aplicación en el
+                // formato de datos solicitado.
                 imageData = try await newImage.loadTransferable(type: Data.self)
             }
         }
     }
-    
-    
+
     var contentStack: some View {
         VStack(alignment: .leading) {
             TextField(text: $title) {
@@ -66,11 +66,11 @@ struct MomentEntryView: View {
             .font(.title.bold())
             .padding(.top, 48)
             Divider()
-            
+
             TextField("Log your small wins", text: $note, axis: .vertical)
                 .multilineTextAlignment(.leading)
-                .lineLimit(5...Int.max) // al menos 5 lineas y todo el texto que deseen
-            
+                .lineLimit(5 ... Int.max) // al menos 5 lineas y todo el texto que deseen
+
             photoPicker
         }
         .padding()
