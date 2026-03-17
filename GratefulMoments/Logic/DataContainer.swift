e61ad5fe25b950a968dd23b6382d1ae59828688e
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-03-16 21:18:13 -0500

    Crea un contenedor de datos. Usa SwiftData para almacenar las entradas. Crea un DataContainer compartido para acceder a SwiftData y proporcionar datos de ejemplo para previsualizaciones y desarrollo.

---
+++ GratefulMoments/Logic/DataContainer.swift
@@ -0,0 +1,8 @@
+//
+//  DataContainer.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 16/3/26.
+//
+
+import Foundation
---
+++ GratefulMoments/Models/Moment.swift
@@ -0,0 +1,8 @@
+//
+//  Moment.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 16/3/26.
+//
+
+import Foundation
