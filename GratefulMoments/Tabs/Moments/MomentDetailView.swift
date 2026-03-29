d37c6fe5c8b283d95f393664c478f37aff5dd8b0
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-03-29 13:46:10 -0500

    Agregar la vista de detalle y la funcionalidad de navegación y eliminación

---
+++ GratefulMoments/Tabs/Moments/MomentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentDetailView.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 29/3/26.
+//
+
+import SwiftUI
+
+struct MomentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentDetailView()
+}
