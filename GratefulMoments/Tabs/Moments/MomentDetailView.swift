fc1b373433fb08fe6a795f8cbe82158bde1f0822
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-03-29 13:47:42 -0500

    Agregar la vista de detalle, la funcionalidad de navegación y eliminar el momento.

--- GratefulMoments/Tabs/Moments/MomentDetailView.swift
+++ GratefulMoments/Tabs/Moments/MomentDetailView.swift
@@ -6,13 +6,68 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MomentDetailView: View {
+    var moment: Moment
+    @State private var showConfirmation = false
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(DataContainer.self) private var dataContainer
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            contentStack
+        }
+        .navigationTitle(moment.title)
+        .toolbar {
+            ToolbarItem(placement: .destructiveAction) {
+                Button {
+                    showConfirmation = true
+                } label: {
+                    Image(systemName: "trash")
+                }
+                .confirmationDialog("Delete Moment", isPresented: $showConfirmation) {
+                    Button("Delete Moment", role: .destructive) {
+                        // Eliminar el momento del contenedor de datos
+                        dataContainer.context.delete(moment)
+                        try? dataContainer.context.save()
+                        // Cerrar la vista de detalles después de eliminar el momento
+                        dismiss()
+                    }
+                } message: {
+                    Text("The moment will be permanently deleted. Earned badges won't be removed.")
+                }
+            }
+        }
+    }
+
+    private var contentStack: some View {
+        VStack(alignment: .leading) {
+            Text(moment.timestamp, style: .date)
+                .font(.subheadline)
+            if !moment.note.isEmpty {
+                Text(moment.note)
+                    .textSelection(.enabled) // El modificador .textSelection permite a los usuarios mantener pulsado un
+                // botón para copiar el texto.
+            }
+            // Comprobar que la imagen exista antes de mostrarla, desempaqueta de forma segura la propiedad de imagen
+            // opcional
+            if let image = moment.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
     }
 }
 
 #Preview {
-    MomentDetailView()
+    NavigationStack {
+        MomentDetailView(moment: .imageSample)
+            .sampleDataContainer()
+    }
 }
