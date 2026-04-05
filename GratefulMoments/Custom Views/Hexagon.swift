83e84ae8c916f43c0412ab7386bcb24bfbb6e86c
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-04-05 16:30:24 -0500

    Añadir la imagen y darle una figura hexagonal

---
+++ GratefulMoments/Custom Views/Hexagon.swift
@@ -0,0 +1,18 @@
+//
+//  Hexagon.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 29/3/26.
+//
+
+import SwiftUI
+
+struct Hexagon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hexagon()
+}
