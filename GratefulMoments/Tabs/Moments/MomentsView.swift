c8f345e91ab9500b7fcbdcf0cb05dbe43372e29d
Alex Salcedo <awsalcedo@sri.gob.ec>
2026-05-13 11:59:53 -0500

    Se aplica transiciones, uso de enumerated()

--- GratefulMoments/Tabs/Moments/MomentsView.swift
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -13,6 +13,8 @@ struct MomentsView: View {
     /// Agrega una query para recuperar los Moments guardados del data store
     @Query(sort: \Moment.timestamp)
     private var moments: [Moment]
+    
+    static let offsetAmount: CGFloat = 70.0
 
     var body: some View {
         NavigationStack {
@@ -41,16 +43,33 @@ struct MomentsView: View {
                     }
                 }
             }
+            /// Utilice .defaultScrollAnchor para configurar la vista de desplazamiento de modo que se muestre el momento más reciente. .defaultScrollAnchor controla la posición del contenido en ScrollView. ScrollAnchorRole ofrece personalización según diferentes roles. Su initialOffset determina la posición inicial de desplazamiento, y su sizeChanges determina los ajustes de diseño cuando cambia el tamaño del contenido, por ejemplo, cuando se agrega o elimina un momento.
+            .defaultScrollAnchor(.bottom, for: .initialOffset)
+            .defaultScrollAnchor(.bottom, for: .sizeChanges)
+            /// Alinea el ScrollView en la parte superior cuando su contenido quepa en la pantalla para que coincida con el comportamiento predeterminado de la lista. En la vista previa sin momentos, agrega un ModelContainer y guarda un nuevo momento para probar el cambio. ScrollAnchorRole.alignment se refiere a cómo una vista de desplazamiento debe alinear su contenido cuando el tamaño de su contenido es menor que el tamaño del contenedor de la vista de desplazamiento.
+            .defaultScrollAnchor(.top, for: .alignment)
             .navigationTitle("Grateful Moments")
         }
     }
 
     private var pathItems: some View {
-        ForEach(moments) { moment in
+        /// Al usar la función .enumerated() en el array moments, se obtiene acceso al índice de cada elemento. La función sin utiliza el índice para crear un valor oscilante, lo que resulta en un patrón en zigzag. Almacenar la magnitud del desplazamiento en una constante estática facilita el ajuste del efecto.
+        ForEach(moments.enumerated(), id: \.0) { index, moment in
             NavigationLink {
                 MomentDetailView(moment: moment)
             } label: {
-                Text(moment.title)
+                if moment == moments.last {
+                    MomentHexagonView(moment: moment, layout: .large)
+                } else {
+                    MomentHexagonView(moment: moment)
+                        .offset(x: sin(Double(index) * .pi / 2) * Self.offsetAmount)
+                }
+            }
+            /// Aplica una transición personalizada a medida que los momentos se desplazan dentro o fuera de la pantalla usando .scrollTransition. Desaparece por completo al desplazarse y reduce el tamaño del hexágono al 80%. La fase de transición se refiere a la etapa en la que se encuentra una vista durante una transición, como aparecer o desaparecer mediante el desplazamiento. La fase de identidad se refiere a cuando el contenido se muestra normalmente, en lugar de estar en transición.
+            .scrollTransition { content, phase in
+                content
+                    .opacity(phase.isIdentity ? 1 : 0)
+                    .scaleEffect(phase.isIdentity ? 1 : 0.8)
             }
         }
     }
@@ -64,4 +83,5 @@ struct MomentsView: View {
 #Preview("No moments") {
     MomentsView()
         .modelContainer(for: [Moment.self])
+        .environment(DataContainer())
 }
