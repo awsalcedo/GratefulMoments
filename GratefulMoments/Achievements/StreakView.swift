204dc8cdc4da61a1a5ec764120b17e309da5f39e
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2026-09-26 09:38:23 -0500

    Calcula y muestra las rachas. Las rachas fomentan la participación constante al recompensar la actividad diaria. Calcula la racha y muéstrala en las pantallas de logros y momentos.

---
+++ GratefulMoments/Achievements/StreakView.swift
@@ -0,0 +1,18 @@
+//
+//  StreakView.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 26/9/26.
+//
+
+import SwiftUI
+
+struct StreakView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StreakView()
+}
---
+++ GratefulMoments/Logic/StreakCalculator.swift
@@ -0,0 +1,8 @@
+//
+//  StreakCalculator.swift
+//  GratefulMoments
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 26/9/26.
+//
+
+import Foundation
